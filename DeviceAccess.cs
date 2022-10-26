f2a940785d1053ef75eeacbb59796a91ee3a4522
Aaron Laney <jaj.laney@gmail.com>
2022-10-26 15:20:20 -0500

    send text after call

--- DeviceAccess.cs
+++ DeviceAccess.cs
@@ -1,100 +1,98 @@
 
 using System.Net.Http.Headers;
-using System.Text.Json;
 using Google.Cloud.PubSub.V1;
-using Grpc.Core;
 using Newtonsoft.Json;
 
 namespace nestalarm
 {
-    public class DeviceAccess
+  public class DeviceAccess
+  {
+    private string projectId;
+    private string accessToken;
+    private string refreshToken;
+    private string oauthClientSecret;
+    private string oauthClientId;
+
+    public DeviceAccess(string accessToken, string refreshToken, string projectId, string oauthClientSecret, string oauthClientId)
     {
-        private string projectId;
-        private string accessToken;
-        private string refreshToken;
-        private string oauthClientSecret;
-        private string oauthClientId;
-
-        public DeviceAccess(string accessToken, string refreshToken, string projectId, string oauthClientSecret, string oauthClientId)
-        {
-            this.accessToken = accessToken;
-            this.refreshToken = refreshToken;
-            this.projectId = projectId;
-            this.oauthClientId = oauthClientId;
-            this.oauthClientSecret = oauthClientSecret;
-        }
-
-        public async Task Authenticate()
-        {
-            HttpClient client = new HttpClient();
-            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            var devicesUri = $"https://smartdevicemanagement.googleapis.com/v1/enterprises/{projectId}/devices";
-            try
-            {
-                using (HttpResponseMessage response = await client.GetAsync(devicesUri))
-                {
-                    if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
-                    {
-                        await RefreshAccessToken();
-                    }
-                }
-            }
-            catch (HttpRequestException ex)
-            {
-                Console.WriteLine("\nException Caught!");
-                Console.WriteLine("Message :{0} ", ex.Message);
-            }
-        }
-
-        public async Task RefreshAccessToken()
-        {
-            var refreshUri = $"https://www.googleapis.com/oauth2/v4/token?client_id={oauthClientId}&client_secret={oauthClientSecret}&refresh_token={refreshToken}&grant_type=refresh_token";
-            HttpClient client = new HttpClient();
-            try
-            {
-                StringContent content = new StringContent("");
-                using (HttpResponseMessage response = await client.PostAsync(refreshUri, content))
-                {
-                    response.EnsureSuccessStatusCode();
-                    string responseBody = await response.Content.ReadAsStringAsync();
-                    dynamic resp = JsonConvert.DeserializeObject(responseBody);
-
-                    if (resp?.access_token != null)
-                    {
-                        this.accessToken = resp.access_token;
-                    }
-                }
-            }
-            catch (HttpRequestException ex)
-            {
-                Console.WriteLine("\nException Caught!");
-                Console.WriteLine("Message :{0} ", ex.Message);
-            }
-        }
-
-        public async Task<bool> CheckForPersonEventAsync(string projectId, string subscriptionId, bool acknowledge)
-        {
-            SubscriptionName subscriptionName = SubscriptionName.FromProjectSubscription(projectId, subscriptionId);
-            SubscriberClient subscriber = await SubscriberClient.CreateAsync(subscriptionName);
-            // SubscriberClient runs your message handle function on multiple
-            // threads to maximize throughput.
-            int personEventCount = 0;
-            Task startTask = subscriber.StartAsync((PubsubMessage message, CancellationToken cancel) =>
-            {
-                string text = System.Text.Encoding.UTF8.GetString(message.Data.ToArray());
-                if (text.Contains("sdm.devices.events.CameraPerson.Person"))
-                {
-                    Interlocked.Increment(ref personEventCount);
-                }                
-                return Task.FromResult(acknowledge ? SubscriberClient.Reply.Ack : SubscriberClient.Reply.Nack);
-            });
-            // Run for 5 seconds.
-            await Task.Delay(5000);
-            await subscriber.StopAsync(CancellationToken.None);
-            // Lets make sure that the start task finished successfully after the call to stop.
-            await startTask;
-            return personEventCount > 0;
-        }
+      this.accessToken = accessToken;
+      this.refreshToken = refreshToken;
+      this.projectId = projectId;
+      this.oauthClientId = oauthClientId;
+      this.oauthClientSecret = oauthClientSecret;
     }
+
+    public async Task Authenticate()
+    {
+      HttpClient client = new HttpClient();
+      client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);
+      client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+      var devicesUri = $"https://smartdevicemanagement.googleapis.com/v1/enterprises/{projectId}/devices";
+      try
+      {
+        using (HttpResponseMessage response = await client.GetAsync(devicesUri))
+        {
+          if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+          {
+            await RefreshAccessToken();
+          }
+        }
+      }
+      catch (HttpRequestException ex)
+      {
+        Console.WriteLine("\nException Caught!");
+        Console.WriteLine("Message :{0} ", ex.Message);
+      }
+    }
+
+    public async Task RefreshAccessToken()
+    {
+      var refreshUri = $"https://www.googleapis.com/oauth2/v4/token?client_id={oauthClientId}&client_secret={oauthClientSecret}&refresh_token={refreshToken}&grant_type=refresh_token";
+      HttpClient client = new HttpClient();
+      try
+      {
+        StringContent content = new StringContent("");
+        using (HttpResponseMessage response = await client.PostAsync(refreshUri, content))
+        {
+          response.EnsureSuccessStatusCode();
+          string responseBody = await response.Content.ReadAsStringAsync();
+          dynamic resp = JsonConvert.DeserializeObject(responseBody);
+
+          if (resp?.access_token != null)
+          {
+            this.accessToken = resp.access_token;
+          }
+        }
+      }
+      catch (HttpRequestException ex)
+      {
+        Console.WriteLine("\nException Caught!");
+        Console.WriteLine("Message :{0} ", ex.Message);
+      }
+    }
+
+    public async Task<bool> CheckForPersonEventAsync(string projectId, string subscriptionId, bool acknowledge)
+    {
+      SubscriptionName subscriptionName = SubscriptionName.FromProjectSubscription(projectId, subscriptionId);
+      SubscriberClient subscriber = await SubscriberClient.CreateAsync(subscriptionName);
+      // SubscriberClient runs your message handle function on multiple
+      // threads to maximize throughput.
+      int personEventCount = 0;
+      Task startTask = subscriber.StartAsync((PubsubMessage message, CancellationToken cancel) =>
+      {
+        string text = System.Text.Encoding.UTF8.GetString(message.Data.ToArray());
+        if (text.Contains("sdm.devices.events.CameraPerson.Person"))
+        {
+          Interlocked.Increment(ref personEventCount);
+        }
+        return Task.FromResult(acknowledge ? SubscriberClient.Reply.Ack : SubscriberClient.Reply.Nack);
+      });
+      // Run for 5 seconds.
+      await Task.Delay(5000);
+      await subscriber.StopAsync(CancellationToken.None);
+      // Lets make sure that the start task finished successfully after the call to stop.
+      await startTask;
+      return personEventCount > 0;
+    }
+  }
 }
--- Program.cs
+++ Program.cs
@@ -4,115 +4,139 @@ using Microsoft.Extensions.Configuration;
 
 namespace nestalarm
 {
-    internal class Program
+  internal class Program
+  {
+    private static bool checking = true;
+    private static string twilioNumber = "+13609681398";
+    private static async Task Main(string[] args)
     {
-        private static async Task Main(string[] args)
+      var configuration = new ConfigurationBuilder()
+          .SetBasePath(Directory.GetCurrentDirectory())
+          .AddJsonFile($"appsettings.json");
+
+      var config = configuration.Build();
+      var accessToken = config["ACCESS_TOKEN"];
+      var refreshToken = config["REFRESH_TOKEN"];
+      var deviceAccessProjectId = config["DEVICE_ACCESS_PROJECT_ID"];
+      var projectId = config["PROJECT_ID"];
+      var oauthClientId = config["OAUTH2_CLIENT_ID"];
+      var oauthClientSecret = config["OAUTH2_CLIENT_SECRET"];
+      var subscriptionId = config["SUBSCRIPTION_ID"];
+      var twilioAccountSid = config["TWILIO_ACCOUNT_SID"];
+      string twilioAuthToken = config["TWILIO_AUTH_TOKEN"];
+      string[] phones = config.GetSection("Phones").GetChildren().Select(x => x.Value).ToArray();
+
+      if (String.IsNullOrEmpty(accessToken) || String.IsNullOrEmpty(refreshToken) || String.IsNullOrEmpty(deviceAccessProjectId))
+      {
+        throw new ApplicationException("Must provde a access token and refresh token");
+      }
+
+      DeviceAccess deviceAccess = new DeviceAccess(accessToken, refreshToken, deviceAccessProjectId, oauthClientSecret, oauthClientId);
+      await deviceAccess.Authenticate();
+
+      string messageSid = "";
+      string answeredPhone = "";
+      while (true)
+      {
+        if (checking)
         {
-            var configuration = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile($"appsettings.json");
-
-            var config = configuration.Build();
-            var accessToken = config["ACCESS_TOKEN"];
-            var refreshToken = config["REFRESH_TOKEN"];
-            var deviceAccessProjectId = config["DEVICE_ACCESS_PROJECT_ID"];
-            var projectId = config["PROJECT_ID"];
-            var oauthClientId = config["OAUTH2_CLIENT_ID"];
-            var oauthClientSecret = config["OAUTH2_CLIENT_SECRET"];
-            var subscriptionId = config["SUBSCRIPTION_ID"];
-            var twilioAccountSid = config["TWILIO_ACCOUNT_SID"];
-            string twilioAuthToken = config["TWILIO_AUTH_TOKEN"];
-            string[] phones = config.GetSection("Phones").GetChildren().Select(x => x.Value).ToArray();
-
-            if (String.IsNullOrEmpty(accessToken) || String.IsNullOrEmpty(refreshToken) || String.IsNullOrEmpty(deviceAccessProjectId))
-            {
-                throw new ApplicationException("Must provde a access token and refresh token");
-            }
-
-            DeviceAccess deviceAccess = new DeviceAccess(accessToken, refreshToken, deviceAccessProjectId, oauthClientSecret, oauthClientId);
-            // await deviceAccess.Authenticate();
-
-            // await CheckEvents(deviceAccess, projectId, subscriptionId);
-            await CallPeople(phones, twilioAccountSid, twilioAuthToken);
+          await CheckEvents(deviceAccess, projectId, subscriptionId);
+          answeredPhone = await CallPhones(phones, twilioAccountSid, twilioAuthToken);
+          if (answeredPhone != "")
+          {
+            checking = false;
+            messageSid = SendText(answeredPhone);
+          }
         }
-
-        private static async Task CheckEvents(DeviceAccess deviceAccess, string projectId, string subscriptionId)
+        else
         {
-            while (true)
-            {
-                TimeSpan start = new TimeSpan(20, 0, 0);
-                TimeSpan midnight = new TimeSpan(24, 0, 0);
-                TimeSpan midnight2 = new TimeSpan(0, 0, 0);
-                TimeSpan end = new TimeSpan(5, 45, 0);
-                TimeSpan now = DateTime.Now.TimeOfDay;
-
-                // if(((now >= start) && (now < midnight)) || (now >= midnight2 && now <= end))
-                // {
-                bool personEvent = await deviceAccess.CheckForPersonEventAsync(projectId, subscriptionId, true);
-                if (personEvent)
-                {
-                    break;
-                }
-                await Task.Delay(5000);
-                // }
-            }
+          CheckTextResponse(messageSid);
+          await Task.Delay(60 * 1000);
         }
-
-        private static async Task CallPeople(string[] phones, string twilioAccountSid, string twilioAuthToken)
-        {
-            bool callAnswered = false;
-            TwilioClient.Init(twilioAccountSid, twilioAuthToken);
-            for (int i = 0; i < phones.Length; i++)
-            {
-                // Call each number twice to bypass "do not disturb" mode.
-                for (int j = 0; j < 2; j++)
-                {
-                    string phone = phones[i];
-                    string callSid = MakeCall(phone);
-                    while (true)
-                    {
-                        CallResource call = CallResource.Fetch(callSid);
-                        Console.WriteLine(call.AnsweredBy);
-                        bool callCompleted = call.Status == CallResource.StatusEnum.Completed;
-                        bool answeredByHuman = call.AnsweredBy == "human";
-                        if (answeredByHuman && callCompleted)
-                        {
-                            callAnswered = true;
-                            break;
-                        } 
-                        else if (callCompleted) 
-                        {
-                            break;
-                        }
-                        await Task.Delay(1000);
-                    }
-
-                    if (callAnswered) {
-                        break;
-                    }
-                }
-
-                if (callAnswered) 
-                {
-                    break;
-                }
-            }
-
-            if (!callAnswered)
-            {
-                // do something? Send a text or log somethingn.
-            }
-        }
-
-        private static string MakeCall(string phone)
-        {
-            var call = CallResource.Create(
-                url: new Uri("http://demo.twilio.com/docs/voice.xml"),
-                to: new Twilio.Types.PhoneNumber("+12565585887"),
-                from: new Twilio.Types.PhoneNumber("+13609681398"),
-                machineDetection: "Enable"
-            );
-            return call.Sid;
-        }
+      }
     }
+
+    private static void CheckTextResponse(string messageSid)
+    {
+      var messages = MessageResource.Read(limit: 20);
+      Console.WriteLine(messages);
+    }
+
+    private static string SendText(string phone)
+    {
+      var message = MessageResource.Create(
+          body: "Join Earth's mightiest heroes. Like Kevin Bacon.",
+          from: new Twilio.Types.PhoneNumber(twilioNumber),
+          to: new Twilio.Types.PhoneNumber(phone)
+      );
+
+      return message.Sid;
+    }
+
+    private static async Task CheckEvents(DeviceAccess deviceAccess, string projectId, string subscriptionId)
+    {
+      while (true)
+      {
+        TimeSpan start = new TimeSpan(0, 0, 0);
+        TimeSpan midnight = new TimeSpan(24, 0, 0);
+        TimeSpan midnight2 = new TimeSpan(0, 0, 0);
+        TimeSpan end = new TimeSpan(24, 0, 0);
+        TimeSpan now = DateTime.Now.TimeOfDay;
+
+        if (((now >= start) && (now < midnight)) || (now >= midnight2 && now <= end))
+        {
+          bool personEvent = await deviceAccess.CheckForPersonEventAsync(projectId, subscriptionId, true);
+          if (personEvent)
+          {
+            break;
+          }
+          await Task.Delay(5000);
+        }
+      }
+    }
+
+    // Calls a list of phone numbers in order and returns the phone number that a human answered, if a human answers.
+    // otherwise returns an empty string.
+    private static async Task<string> CallPhones(string[] phones, string twilioAccountSid, string twilioAuthToken)
+    {
+      TwilioClient.Init(twilioAccountSid, twilioAuthToken);
+      for (int i = 0; i < phones.Length; i++)
+      {
+        string phone = phones[i];
+        // Call each number twice to bypass "do not disturb" mode.
+        for (int j = 0; j < 2; j++)
+        {
+          string callSid = MakeCall(phone);
+          while (true)
+          {
+            CallResource call = CallResource.Fetch(callSid);
+            bool callCompleted = call.Status == CallResource.StatusEnum.Completed;
+            bool answeredByHuman = call.AnsweredBy == "human";
+            if (answeredByHuman)
+            {
+              return phone;
+            }
+            else if (callCompleted)
+            {
+              break;
+            }
+            await Task.Delay(1000);
+          }
+        }
+      }
+
+      return "";
+    }
+
+    private static string MakeCall(string phone)
+    {
+      var call = CallResource.Create(
+          url: new Uri("http://demo.twilio.com/docs/voice.xml"),
+          to: new Twilio.Types.PhoneNumber(phone),
+          from: new Twilio.Types.PhoneNumber(twilioNumber),
+          machineDetection: "Enable"
+      );
+      return call.Sid;
+    }
+  }
 }
